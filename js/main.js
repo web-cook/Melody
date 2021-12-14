89bf618706a8c29c06d8c8696f7fd268f081ddc8
Vladimir <web-cook@inbox.ru>
2021-12-14 19:32:58 +0300

    подсветка ссылок при наведении на схему

--- js/main.js
+++ js/main.js
@@ -3,10 +3,52 @@ $(document).ready(function () {
     let floorPath = $('.home-image path');
     let counterUp = $('.counter-up');
     let counterDown = $('.counter-down');
-    let usCurrentFloor = '';
     let modal = $('.modal');
     let modalClose = $('.modal-close-button');
     let viewFlatsButton = $('.view-flats');
+
+    let flat = $('.flats path');
+    let description = $('.flat-number');
+    let linkFlat = $('.flat-link');
+    
+    //при наведении на схему квартиры
+    flat.on('mouseover', function () {
+        currentFlat = $(this).attr('data-flat');
+        for (let el of description) {
+            if (el.parentNode.classList.contains('flat-link-jhover')) {
+                el.parentNode.classList.toggle('flat-link-jhover');
+            }
+            if (el.innerHTML === currentFlat) {
+                el.parentNode.classList.toggle('flat-link-jhover');             
+            }
+        }
+    })
+
+    flat.on('mouseout', function () {
+        for (let el of description) {
+            if (el.parentNode.classList.contains('flat-link-jhover')) {
+                el.parentNode.classList.toggle('flat-link-jhover');
+            }
+        }
+    })
+
+    // при наведении на ссылку с описанием квартиры
+    linkFlat.on('mouseover', function () {
+        currentLink = $(this).children()[0].innerHTML
+        for (let el of flat) {
+            if (el.getAttribute('data-flat') === currentLink) {
+                el.classList.toggle('flats-jhover');
+            }
+        }
+    })
+
+    linkFlat.on('mouseout', function () {
+        for (let el of flat) {
+            if (el.classList.contains('flats-jhover')) {
+                el.classList.toggle('flats-jhover');
+            }
+        }
+    })
 
     floorPath.on('click', toggleModal);
     modalClose.on('click', toggleModal);
